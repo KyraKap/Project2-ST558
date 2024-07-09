0c54b2d6fa7edba8cc37c582a7351cf055cf8a1d
Kyra Kapsaskis <kyrakapsaskis@Kyras-Air-3.lan>
2024-07-08 20:56:40 -0400

    improvements to query function

--- server.R
+++ server.R
@@ -1,5 +1,10 @@
-library(shiny) 
+library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output, session) { 
+  output$chapel_hill_plot <- renderPlot(
+    #code that will return a plot
+    ggplot(chapel_hill_data, aes(x = chapel_hill_data$day_length,y = chapel_hill_data$sunrise)) + geom_point()
+  )
 
 })
--- ui.R
+++ ui.R
@@ -1,18 +1,43 @@
 library(shiny) 
 
 ui <- fluidPage(
-  navbarPage("Finding the time of Sunrise and Sunset", id = NULL, selected= NULL, position= "static-top", header = "this is my header", inverse=TRUE, fluid=TRUE),
+  
+  # top of page - title
+  
+  navbarPage("Finding the time of Sunrise and Sunset", id = NULL, selected= NULL, position= "static-top", fluid=TRUE),
 
+    sidebarLayout(
+      
+      sidebarPanel("hello"),
+        
+      )
+    )
+  
+    # panel of tabs across the top
+# _______________________________________ 
     tabsetPanel(
+      
+      # tab 1 - name of the tab and what it says on the tab
+      
       tabPanel("About", "contents"),
+      
+      # attempting to put plot here
+      
+        plotOutput("chapel_hill_plot"), 
+      
       tabPanel("Data Download", "contents", dateInput("date 1", label = "INSERT DATE HERE", format = "yyyy-mm-dd"), varSelectInput(
-        "variable1",
-        label = "data for my location",
-        chapel_hill_data$sunrise),
-        ),
+        "variable1", label = "data for my location", chapel_hill_data$sunrise),
+      
       tabPanel("Data Exploration", "contents"),
       
-  
+      # Create a spot for the plot
+      
+      ),
+      
+    mainPanel(
+         
+      )
   )
+# _______________________________________ 
 )
 
