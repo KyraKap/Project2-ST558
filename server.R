c9b17ae4cb373e672595f360e9567559e66211e2
Kyra Kapsaskis <kyrakapsaskis@Kyras-Air-3.lan>
2024-07-09 01:02:00 -0400

    still trying to get the data to work

--- server.R
+++ server.R
@@ -4,7 +4,6 @@ library(ggplot2)
 shinyServer(function(input, output, session) { 
   output$chapel_hill_plot <- renderPlot(
     #code that will return a plot
-    ggplot(chapel_hill_data, aes(x = chapel_hill_data$day_length,y = chapel_hill_data$sunrise)) + geom_point()
   )
 
 })
--- ui.R
+++ ui.R
@@ -9,6 +9,8 @@ ui <- fluidPage(
     sidebarLayout(
       
       sidebarPanel("hello",
+                   h3("API Query"),
+                   selectInput(capital_cities$Capital.City, "Capital City", capital_cities$Capital.City),
                    sliderInput("bins", "Number of bins:", min=1, max=50, 
                                value=30),
                    textInput("capital_plot", "My Capitals Plot:", value="Histogram")
@@ -27,7 +29,8 @@ ui <- fluidPage(
       
       # attempting to put plot here
       
-      tabPanel("Data Download"),
+      tabPanel("Data Download"
+               ),
       tabPanel("Data Exploration", "contents")
     )
   )
