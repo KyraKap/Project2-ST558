8b0b99bd745104b9a68c91828d5fab51b875a4be
Kyra Kapsaskis <kyrakapsaskis@Kyras-Air-3.lan>
2024-07-08 23:19:18 -0400

    progress on structure of app

--- ui.R
+++ ui.R
@@ -8,36 +8,26 @@ ui <- fluidPage(
 
     sidebarLayout(
       
-      sidebarPanel("hello"),
-        
-      )
-    )
+      sidebarPanel(("hello"),
+                   sliderInput("bins", "Number of bins:", min=1, max=50, 
+                               value=30),
+                   textInput("capital_plot", "My Capitals Plot:", value="Histogram")
+      ),
+    ),
+      
+      mainPanel("hello"),
   
-    # panel of tabs across the top
-# _______________________________________ 
-    tabsetPanel(
+      
+      tabsetPanel(
       
       # tab 1 - name of the tab and what it says on the tab
       
-      tabPanel("About", "contents"),
+        tabPanel("About", "contents"),
       
       # attempting to put plot here
       
-        plotOutput("chapel_hill_plot"), 
-      
-      tabPanel("Data Download", "contents", dateInput("date 1", label = "INSERT DATE HERE", format = "yyyy-mm-dd"), varSelectInput(
-        "variable1", label = "data for my location", chapel_hill_data$sunrise),
-      
-      tabPanel("Data Exploration", "contents"),
-      
-      # Create a spot for the plot
-      
-      ),
-      
-    mainPanel(
-         
+        tabPanel("Data Download"),
+        tabPanel("Data Exploration", "contents"),
       )
-  )
-# _______________________________________ 
-)
+
 
